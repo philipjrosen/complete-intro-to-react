3bc783c02643bff30feab32162d860ff375488f1
Philip Rosen <philipjrosen@gmail.com>
2018-04-08 20:55:09 -0700

    refactor Search to be a stateful component with a text input

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "initial state"
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
