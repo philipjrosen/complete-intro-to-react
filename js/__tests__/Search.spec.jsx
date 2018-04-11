f3e9bf1f2df0256fbe2efaf6b0c299901c6166d6
Philip Rosen <philipjrosen@gmail.com>
2018-04-10 19:49:29 -0700

    add test to check correct number of shows is rendered

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
+import preload from "../../data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
 test("Search renders correctly", () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
+});
+
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
